7463e3dd505fa1e46d8ef624692447923d2e200b
epenet <6771947+epenet@users.noreply.github.com>
2026-03-13 09:31:56 +0100

    Add wrappers for fan (#43)
    * Add wrappers for fan
    
    * One more
    
    * Coverage

---
+++ src/tuya_device_handlers/device_wrapper/fan.py
@@ -0,0 +1,52 @@
+"""Tuya device wrapper."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any
+
+from ..type_information import IntegerTypeInformation
+from .common import DPCodeEnumWrapper
+from .extended import DPCodeRemappedIntegerWrapper
+
+if TYPE_CHECKING:
+    from tuya_sharing import CustomerDevice  # type: ignore[import-untyped]
+
+
+class FanSpeedIntegerWrapper(DPCodeRemappedIntegerWrapper):
+    """Wrapper for fan speed DP code (from an integer).
+
+    Contrary to the standard DPCodePercentageWrapper we start the range at 1.
+    """
+
+    def __init__(
+        self, dpcode: str, type_information: IntegerTypeInformation
+    ) -> None:
+        """Init FanSpeedIntegerWrapper."""
+        super().__init__(dpcode, type_information, target_min=1, target_max=100)
+
+
+class FanSpeedEnumWrapper(DPCodeEnumWrapper[int]):
+    """Wrapper for fan speed DP code (from an enum)."""
+
+    def read_device_status(self, device: CustomerDevice) -> int | None:
+        """Get the current speed as a percentage."""
+        if (value := self._read_dpcode_value(device)) is None:
+            return None
+
+        list_len = len(self.options)
+        list_position = self.options.index(value) + 1
+        return (list_position * 100) // list_len
+
+    def _convert_value_to_raw_value(
+        self, device: CustomerDevice, value: Any
+    ) -> Any:
+        """Convert a Home Assistant value back to a raw device value."""
+        list_len = len(self.options)
+
+        for offset, speed in enumerate(self.options):
+            list_position = offset + 1
+            upper_bound = (list_position * 100) // list_len
+            if value <= upper_bound:
+                return speed
+
+        return self.options[-1]
